4936e8f4c6c532432327b923618de152c9ba7f63
mehmet <nalsaatem@gmail.com>
2022-10-28 20:04:17 -0400

    new reverse

---
+++ src/reverse.java
@@ -0,0 +1,13 @@
+public class reverse {
+    public static void main(String[] args) {
+
+
+    String name ="mehmet";
+    StringBuilder rev= new StringBuilder();
+
+        rev.append(name);
+        rev.reverse();
+        System.out.println(rev);
+}
+}
+
